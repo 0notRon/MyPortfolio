8904304f7d324b1876e96a8fdbc7bdf1c4940458
Hezrone Ocampo <153799379+0notRon@users.noreply.github.com>
2025-05-25 20:21:23 +0800

    feat: Update introduction text in Home component

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -9,11 +9,11 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I am Hezrone
+            Hi! I’m Hezrone
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            BAKLA
+            A student, freelance artist, and tech enthusiast with a love for turning ideas into visuals and digital experiences.
           </p>
           <div className="flex justify-center space-x-4">
             <a
